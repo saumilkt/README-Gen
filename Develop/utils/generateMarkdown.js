30e0847dcdc4d12daf2c262054a2cc4daec83787
saumilkt <saumilkt@gmail.com>
2021-08-16 00:02:59 -0700

    adding fxn for generating license badge, description, and table of contents

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,36 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+// creates license badge if license is chosen
+const addLicenseBadge = license => {
+  if (license) {
+      return `![${license} License](https://img.shields.io/badge/license-${license.split(' ').join('%20')}-blue)
 `;
-}
+  } else {
+      return '';
+  }
+};
+// creates description section
+const createDescription = (title, description, link) => {
+  if (link) {
+      return `${description}
+          
+View the deployed page at [${title}](${link}).`;
+  } else {
+      return `${description}`;
+  }
+};
+// creates table of contents
+const createTableOfContents = contentsArr => {
+  // creates contents list items based on user selection
+  let contentsList = '';
+  contentsArr.forEach((item) => {
 
-module.exports = generateMarkdown;
+      // indents 'Screenshots' list item
+      if (item.content && item.header === 'Screenshots') {
+      contentsList += `   * [${item.header}](#${(item.header).toLowerCase()})
+`;
+      } else if (item.content) {
+          contentsList += `* [${item.header}](#${(item.header).toLowerCase().split(' ').join('-')})
+`;
+      }
+  });
+  return contentsList;
+};
